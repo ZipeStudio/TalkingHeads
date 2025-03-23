044158ea949e3243b19fdb8b9e1b3a04da129fc0
ZipeStudio <faladorgame@gmail.com>
2025-03-23 17:55:20 +0200

    new commit

--- src/main/java/me/zipestudio/talkingheads/client/THClient.java
+++ src/main/java/me/zipestudio/talkingheads/client/THClient.java
@@ -10,10 +10,6 @@ public class THClient implements ClientModInitializer {
 
         FabricLoader instance = FabricLoader.getInstance();
 
-        if (!instance.getModContainer("plasmovoice").get().getMetadata().getVersion().getFriendlyString().startsWith("2.1")) {
-            return;
-        }
-
         if (instance.isModLoaded("plasmovoice")) {
             su.plo.voice.api.client.PlasmoVoiceClient.getAddonsLoader().load(new PlasmoVoiceAddon());
         }
